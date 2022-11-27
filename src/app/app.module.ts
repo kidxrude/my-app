a42a9b419fd34d7efc919b3e5bf5c923e2fbf771
Rudie <rudie@bicat.com>
2022-11-27 12:25:16 +0100

    basic list of sessions based upon local assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SessionListComponent } from './session-list/session-list.component';
+import { MapComponent } from './map/map.component';
+import { SessionService } from './services/session.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
